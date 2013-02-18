6121a44ea680c3edb73a75a410090cb1b2f6d173
Juha Yrjölä <juha.yrjola@iki.fi>
2013-02-18 12:41:02 +0200

    Add __unicode__() methods for models

--- models.py
+++ models.py
@@ -5,8 +5,16 @@ class Point(EmbeddedDocument):
     coords = GeoPointField(required=True)
     events = ListField(StringField(required=True))
 
+    def __unicode__(self):
+        return "%s (%s) at %f, %f" % (self.timestamp, ', '.join(self.events),
+                self.coords[0], self.coords[1])
+
 class Plow(Document):
     id = IntField(unique=True, primary_key=True)
     first_loc = EmbeddedDocumentField(Point)
     last_loc = EmbeddedDocumentField(Point)
     points = ListField(EmbeddedDocumentField(Point))
+
+    def __unicode__(self):
+        return "%d (first %s; last %s; %d points)" % (self.id,
+                self.first_loc, self.last_loc, len(self.points))
